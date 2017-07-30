497458aeecca6a3f90852c2b9f587ac01b2a2351
Castle <castlez93@gmail.com>
2017-07-30 15:06:09 -0700

    this is a steaming pile of shit

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import urllib
 import json
+from flask import request
 
 
 from flask import Flask
 app = Flask(__name__)
+version = "0.1"
 
-@app.route("/")
+def get_page():
+    return "<html><body><a>version {}</a><br><br>".format(version) + \
+              '<form action="." method="POST"><input type="text" name="text"><input type="submit" name="my-form" value="Send"></form>' + \
+              "<br><br>"
+
+@app.route("/app", methods=['GET'])
 def main():
-   with urllib.request.urlopen('https://api.deckbrew.com/mtg/cards?name=memnarch') as response:
-       j = json.loads(response.read())[0]
-       # json_string = json.dumps(j, indent=4, sort_keys=True)
-       img = "https://image.deckbrew.com/mtg/multiverseid/220532.jpg"
-       img_html = "<img src=\"" + img + "\"/>"
-       return "<html><body>" + img_html +"</body></html>"
+    return get_page()
 
 
+@app.route('/app', methods=['POST'])
+def my_form_post():
+    # card_name = "memnarch"
+    card_name = request.form['text']
+    with urllib.request.urlopen("https://api.deckbrew.com/mtg/cards?name={}".format(card_name)) as response:
+        j = json.loads(response.read())
+        multiverse_id = "220532"
+        img = "https://image.deckbrew.com/mtg/multiverseid/{}.jpg".format(multiverse_id)
+        img_html = "<img src=\"" + img + "\"/>"
+        return get_page() + "<br><br>" + \
+               img_html + \
+               "</body></html>"
 
 if __name__ == '__main__':
   app.run()
